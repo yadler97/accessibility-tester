edc5aa71ec7f4425de6dcbb6e42a3cb8e4f0cbac
Yannick Adler <yannick.adler@yahoo.de>
2020-09-11 23:50:00 +0200

    check_buttons implementation

--- bs4_test.py
+++ bs4_test.py
@@ -14,6 +14,7 @@ DEFAULT_BACKGROUND_COLOR = "#FFFFFF"
 DEFAULT_BUTTON_COLOR = "#EFEFEF"
 
 # 3.1.1 H57
+# Document language missing
 def check_lang_attribute():
     lang_attribute = soup.find("html").get_attribute_list("lang")[0]
     if not lang_attribute == None:
@@ -22,6 +23,7 @@ def check_lang_attribute():
         print("Lang attribute is missing")
 
 # 1.1.1 H37
+# Missing alternative text
 def check_alt_texts():
     img_tags = soup.find_all("img")
     for img in img_tags:
@@ -32,13 +34,14 @@ def check_alt_texts():
             print("Alt text is missing")
 
 # 1.3.1 H44
+# Missing form label
 def check_input_label():
     # get all input and label tags
     input_tags = soup.find_all("input")
     label_tags = soup.find_all("label")
     for input_tag in input_tags:
         # exclude input tags of type hidden or submit
-        if "type" in input_tag.attrs and not input_tag['type'] == "hidden" and not input_tag['type'] == "submit":
+        if "type" in input_tag.attrs and not input_tag['type'] == "hidden" and not input_tag['type'] == "submit" and not input_tag['type'] == "button" and not input_tag['type'] == "reset":
             # check if input tag uses aria label
             if not "aria-label" in input_tag.attrs and not "aria-labelledby" in input_tag.attrs:
                 # check if input tag has a corresponding label tag
@@ -55,12 +58,14 @@ def check_input_label():
                 print("Input labelled with aria tag")
 
 # 1.4.3 G18 & G145 (& 148)
+# Low contrast
 def check_color_contrast():
     # exclude script, style, title and empty tags as well as doctype and comments
     texts_on_page = extract_texts()
     input_tags = soup.find_all("input")
     text_tags = texts_on_page + input_tags
     for text in text_tags:
+        #print(text.string)
         # exclude invisible texts
         tag_visible = get_css_attribute_value(text, "display")
         if not tag_visible == "none" and (not text.name == "input" or (text.name == "input" and "type" in text.attrs and not text['type'] == "hidden")):
@@ -89,9 +94,36 @@ def check_color_contrast():
                 else:
                     print("Contrast does not meet minimum requirements")
 
+# 1.1.1 & 2.4.4
+# Empty button
+# TODO
+def check_buttons():
+    input_tags = soup.find_all("input", type=["submit", "button", "reset"])
+    button_tags = soup.find_all("button")
+
+    for input_tag in input_tags:
+        if "value" in input_tag.attrs and not input_tag['value'] == "":
+            print("Button has content")
+        else:
+            print("Button is empty")
+
+    for button_tag in button_tags:
+        texts_in_button_tag = button_tag.findAll(text=True)
+        if not texts_in_button_tag == [] or ("aria-label" in button_tag.attrs and not button_tag["aria-label"] == ""):
+            print("Button has content")
+        else:
+            print("Button is empty")
+
+# 2.4.4 G91 & H33 & ARIA7 & ARIA8
+# Empty link
+# TODO
+def check_links():
+    link_tags = soup.find_all("a")
+
 def get_stylesheets():
     for styletag in soup.findAll('style'):
-        stylesheets.append(cssutils.parseString(styletag.string))
+        if not styletag.string == None:
+            stylesheets.append(cssutils.parseString(styletag.string))
 
     for linktag in soup.findAll('link', rel='stylesheet'):
         if linktag['href'].startswith("/") or not linktag['href'].split("/")[0].__contains__(":"):
@@ -103,34 +135,42 @@ def get_stylesheets():
             cssreq = requests.get(linktag['href'])
         stylesheets.append(cssutils.parseString(cssreq.content))
 
-def get_rules(sheet, css_list):
+def get_rules(css_list):
     rules = []
-    for rule in sheet:
-        if rule.type == rule.STYLE_RULE:
-            for single_rule in rule.selectorText.split(","):
-                starting_rule = True
-                iterable_css_list = css_list[:]
-                iterable_single_rule = single_rule.split(" ")
-                is_correct_full = True
-                for single_rule_part in reversed(iterable_single_rule):
-                    is_correct = False
-                    for css_list_rule in iterable_css_list:
-                        if single_rule_part in ["." + class_name for class_name in css_list_rule[1]] or single_rule_part == css_list_rule[0] or single_rule_part == "#" + css_list_rule[2] or single_rule_part in [css_list_rule[0] + "." + class_name for class_name in css_list_rule[1]]:
-                            is_correct = True
-                            starting_rule = False
-                            del iterable_css_list[0]
+    for sheet in stylesheets:
+        for rule in sheet:
+            if rule.type == rule.STYLE_RULE:
+                for single_rule in rule.selectorText.split(","):
+                    starting_rule = True
+                    iterable_css_list = css_list[:]
+                    iterable_single_rule = single_rule.split(" ")
+                    is_correct_full = True
+                    for single_rule_part in reversed(iterable_single_rule):
+                        is_correct = False
+                        for css_list_rule in iterable_css_list:
+                            if single_rule_part in ["." + class_name for class_name in css_list_rule[1]] or single_rule_part == css_list_rule[0] or single_rule_part == "#" + css_list_rule[2] or single_rule_part in [css_list_rule[0] + "." + class_name for class_name in css_list_rule[1]] or single_rule_part == "*":
+                                is_correct = True
+                                starting_rule = False
+                                del iterable_css_list[0]
+                                break
+                            elif starting_rule:
+                                break
+                            if not len(iterable_single_rule) == 0:
+                                del iterable_single_rule[0]
+                        if not is_correct:
+                            is_correct_full = False
                             break
-                        elif starting_rule:
-                            break
-                        if not len(iterable_single_rule) == 0:
-                            del iterable_single_rule[0]
-                    if not is_correct:
-                        is_correct_full = False
-                        break
-                if is_correct_full:
-                    rules.append(rule)
+                    if is_correct_full:
+                        rule_to_be_added = rule
+                        rule_to_be_added.selectorText = single_rule
+                        rules.append(rule_to_be_added)
 
+    rules.sort(reverse=True, key=get_specificity)
     return rules
+
+def get_specificity(rule):
+    specificity = rule.selectorList[0].specificity
+    return specificity[0] * 1000 + specificity[1] * 100 + specificity[2] * 10 + specificity[3]
 
 def extract_texts():
     soup2 = soup
@@ -160,7 +200,7 @@ def extract_texts():
 def get_text_color(text_color, text):
     if text_color == "initial":
         return convert_color(DEFAULT_TEXT_COLOR)
-    elif text_color == "inherit":
+    elif text_color == "inherit" or text_color == "transparent" or text_color == "none":
         return convert_color(get_text_color(get_css_attribute_value(text.parent, "color"), text.parent))
     elif not text_color == None:
         return convert_color(text_color)
@@ -197,29 +237,10 @@ def get_css_attribute_value(text, attribute):
 
     class_list = get_css_class_list(text)
 
-    # TODO IDs have the second highest specificity
-    if "id" in text.attrs:
-        for sheet in stylesheets:
-            rules = get_rules(sheet, class_list)
-            for rule in rules:
-                if not rule.style[attribute] == "":
-                    return rule.style[attribute]
-
-    # classes have the third highest specificity
-    if "class" in text.attrs:
-        for sheet in stylesheets:
-            rules = get_rules(sheet, class_list)
-            for rule in rules:
-                if not rule.style[attribute] == "":
-                    return rule.style[attribute]
-
-    # html tags have the least highest specificity
-    tag_name = text.name
-    for sheet in stylesheets:
-        rules = get_rules(sheet, class_list)
-        for rule in rules:
-            if not rule.style[attribute] == "":
-                return rule.style[attribute]
+    rules = get_rules(class_list)
+    for rule in rules:
+        if not rule.style[attribute] == "":
+            return rule.style[attribute]
 
     # * rule TODO
     
@@ -231,10 +252,10 @@ def get_background_color_attribute(text):
         background_color = get_css_attribute_value(text, "background")
     if background_color == None and not text.name == "body" and not text.name == "input":
         background_color = get_background_color_attribute(text.parent)
-    elif background_color == None and "type" in text.attrs and text["type"] == "submit":
-        background_color = DEFAULT_BUTTON_COLOR
+    elif background_color == None and "type" in text.attrs and text["type"] == "submit" and text["type"] == "button" and text["type"] == "reset":
+        return DEFAULT_BUTTON_COLOR
     elif background_color == None:
-        background_color = DEFAULT_BACKGROUND_COLOR
+        return DEFAULT_BACKGROUND_COLOR
     if background_color.startswith("url"):
         background_color = get_background_color_attribute(text.parent)
     return background_color
@@ -289,7 +310,9 @@ except requests.exceptions.MissingSchema:
     raise SystemExit
 
 get_stylesheets()
-check_color_contrast()
+#check_color_contrast()
 check_input_label()
 check_lang_attribute()
 check_alt_texts()
+check_links()
+check_buttons()
