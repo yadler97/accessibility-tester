64b9d4007b38ccb3298650c76f8cbab93c169c9d
Yannick Adler <yannicik.adler@telekom.de>
2021-03-03 17:01:16 +0100

    improvements for link loop

--- selenium_rewrite.py
+++ selenium_rewrite.py
@@ -1,20 +1,26 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 import sys
 import validators
 import urllib.parse
 import time
+import os
 
 class VAT:
-    def __init__(self, url):
+    def __init__(self, url, required_degree):
         options = Options()
         options.headless = True
 
+        self.url = url
+        self.required_degree = required_degree
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(1120, 550)
         self.driver.get(url)
         self.page = BeautifulSoup(self.driver.page_source, "html.parser")
+        self.correct = {"doc_language":0, "alt_texts":0, "input_labels":0, "empty_buttons":0}
+        self.wrong = {"doc_language":0, "alt_texts":0, "input_labels":0, "empty_buttons":0}
 
         #self.link_list = []
 
@@ -25,20 +31,34 @@ class VAT:
         #        if joined_url not in self.link_list:
         #            self.link_list.append(joined_url)
 
-        self.link_list = self.driver.find_elements_by_tag_name("a")
+        self.visited_links = []
 
     def test_subpages(self):
-        for i in range(len(self.link_list)):
+        self.page = BeautifulSoup(self.driver.page_source, "html.parser")
+        print("\n" + self.driver.current_url + "\n---------------------")
+        self.check_doc_language()
+        self.check_alt_texts()
+        self.check_input_labels()
+        self.check_buttons()
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.driver.get_screenshot_as_file(dir_path + "/screenshots/" + str(time.time()) + ".png")
+
+        self.visited_links.append(self.driver.current_url)
+
+        link_list = self.driver.find_elements_by_tag_name("a")
+        for i in range(len(link_list)):
             self.driver.execute_script("elements = document.getElementsByTagName('a'); for (var element of elements) {element.setAttribute('target', '')}")
             link = self.driver.find_elements_by_tag_name("a")[i]
+            if not link.is_displayed() or link.get_attribute("href") == "" or link.get_attribute("href") == None or str(urllib.parse.urljoin(self.url, link.get_attribute("href"))) == self.driver.current_url:
+                continue
             link.click()
-            #self.driver.get_screenshot_as_file(str(i) + ".png")
-            self.page = BeautifulSoup(self.driver.page_source, "html.parser")
-            print("\n" + self.driver.current_url + "\n---------------------")
-            self.check_doc_language()
-            self.check_alt_texts()
-            self.check_input_labels()
-            self.check_buttons()
+            if self.driver.current_url in self.visited_links or not self.url in self.driver.current_url:
+                self.driver.back()
+                continue
+
+            self.test_subpages()
+
             self.driver.back()
 
     # 3.1.1 H57
@@ -52,16 +72,14 @@ class VAT:
         lang_attr = self.page.find("html").get_attribute_list("lang")[0]
         if not lang_attr == None:
             print("Document language is set")
-            return {"category":"doc_language","correct":1,"false":0}
+            self.correct["doc_language"] += 1
         else:
             print("Document language is missing")
-            return {"category":"doc_language","correct":0,"false":1}
+            self.wrong["doc_language"] += 1
 
     # 1.1.1 H37
     # Missing alternative text
     def check_alt_texts(self):
-        correct = 0
-        false = 0
         # get all img elements
         img_elements = self.page.find_all("img")
         for img_element in img_elements:
@@ -69,18 +87,14 @@ class VAT:
             alt_text = img_element.get_attribute_list('alt')[0]
             if not alt_text == None:
                 print("Alt text is correct")
-                correct += 1
+                self.correct["alt_texts"] += 1
             else:
                 print("Alt text is missing")
-                false += 1
-        
-        return {"category":"alt_texts","correct":correct,"false":false}
+                self.wrong["alt_texts"] += 1
 
     # 1.3.1 H44 & ARIA16
     # Missing form label
     def check_input_labels(self):
-        correct = 0
-        false = 0
         # get all input and label elements
         input_elements = self.page.find_all("input")
         label_elements = self.page.find_all("label")
@@ -90,11 +104,11 @@ class VAT:
                 # check if input is of type image and has a alt text that is not empty
                 if input_element['type'] == "image" and "alt" in input_element.attrs and not input_element['alt'] == "":
                     print("Input of type image labelled with alt text")
-                    correct += 1
+                    self.correct["input_labels"] += 1
                 # check if input element uses aria-labelledby
                 elif "aria-labelledby" in input_element.attrs and not input_element['aria-labelledby'] == "":
                     print("Input labelled with aria-labelledby attribute")
-                    correct += 1
+                    self.correct["input_labels"] += 1
                 else:
                     # check if input element has a corresponding label element
                     label_correct = False
@@ -104,18 +118,14 @@ class VAT:
                             label_correct = True
                     if label_correct == True:
                         print("Input labelled with label element")
-                        correct += 1
+                        self.correct["input_labels"] += 1
                     else:
                         print("Input not labelled at all")
-                        false += 1
-
-        return {"category":"input_labels","correct":correct,"false":false}
+                        self.wrong["input_labels"] += 1
 
     # 1.1.1 & 2.4.4
     # Empty button
     def check_buttons(self):
-        correct = 0
-        false = 0
         # get all buttons and input elements of the types submit, button and reset
         input_elements = self.page.find_all("input", type=["submit", "button", "reset"])
         button_elements = self.page.find_all("button")
@@ -124,45 +134,57 @@ class VAT:
             # check if input element has a value attribute that is not empty
             if "value" in input_element.attrs and not input_element['value'] == "":
                 print("Button has content")
-                correct += 1
+                self.correct["empty_buttons"] += 1
             else:
                 print("Button is empty")
-                false += 1
+                self.wrong["empty_buttons"] += 1
 
         for button_element in button_elements:
             # check if the button has content or a title
             texts = button_element.findAll(text=True)
             if not texts == [] or ("title" in button_element.attrs and not button_element["title"] == ""):
                 print("Button has content")
-                correct += 1
+                self.correct["empty_buttons"] += 1
             else:
                 print("Button is empty")
-                false += 1
+                self.wrong["empty_buttons"] += 1
 
-        return {"category":"empty_buttons","correct":correct,"false":false}
+    def calculate_result(self):
+        # calculate correct and false implementations
+        correct = sum(self.correct.values())
+        false = sum(self.wrong.values())
+        print("---------------------")
+        print("Correct:", correct)
+        for category in self.correct:
+            print(" ", category + ":", self.correct[category])
+        print("Errors:", false)
+        for category in self.wrong:
+            print(" ", category + ":", self.wrong[category])
+        print("Ratio (correct to total):", round(correct/(correct+false), 2), "\n")
+
+        # check if ratio correct/total reaches wanted minimum value
+        if correct/(correct+false) >= self.required_degree:
+            print("Accessibility test successful - can deploy")
+        else:
+            raise Exception("Too many accessibility errors - try fix them!")
 
 def main():
     url = sys.argv[1]
     required_degree = float(sys.argv[2])
 
     if not validators.url(url):
-        print("ERROR: Invalid URL")
-        exit(1)
+        raise Exception("Invalid URL")
 
     if not 0 <= required_degree <= 1:
-        print("ERROR: Accessibility level must be between 0 and 1")
-        exit(1)
+        raise Exception("Accessibility level must be between 0 and 1")
 
-    vat = VAT(url)
-
-    vat.check_doc_language()
-    vat.check_alt_texts()
-    vat.check_input_labels()
-    vat.check_buttons()
+    vat = VAT(url, required_degree)
 
     vat.test_subpages()
 
     vat.driver.quit()
+
+    vat.calculate_result()
 
 if __name__ == "__main__":
     main()
